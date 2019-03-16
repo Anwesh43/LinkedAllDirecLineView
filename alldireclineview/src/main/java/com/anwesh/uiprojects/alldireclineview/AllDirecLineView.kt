94045b62a5d8975f43f2c326499f3407a9ae329e
Anwesh43 <anweshthecool0@gmail.com>
2019-03-16 14:39:20 +0530

    adding static create method

--- alldireclineview/src/main/java/com/anwesh/uiprojects/alldireclineview/AllDirecLineView.kt
+++ alldireclineview/src/main/java/com/anwesh/uiprojects/alldireclineview/AllDirecLineView.kt
@@ -209,4 +209,13 @@ class AllDirecLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : AllDirecLineView {
+            val view : AllDirecLineView = AllDirecLineView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
