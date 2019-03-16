f90efe30c3adda948f887c78242ffdeba296166b
Anwesh43 <anweshthecool0@gmail.com>
2019-03-16 14:24:56 +0530

    adding ADLNode class

--- alldireclineview/src/main/java/com/anwesh/uiprojects/alldireclineview/AllDirecLineView.kt
+++ alldireclineview/src/main/java/com/anwesh/uiprojects/alldireclineview/AllDirecLineView.kt
@@ -114,8 +114,52 @@ class AllDirecLineView(ctx : Context) : View(ctx) {
 
         fun stop() {
             if (animated) {
-                animated = false 
+                animated = false
             }
+        }
+    }
+
+    data class ADLNode(var i : Int, val state : State = State()) {
+
+        private var next : ADLNode? = null
+        private var prev : ADLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = ADLNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ADLNode {
+            var curr : ADLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawADLNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
         }
     }
 }
