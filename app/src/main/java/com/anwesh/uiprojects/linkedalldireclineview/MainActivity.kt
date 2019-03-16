bae5690bde6a32821544e6f931237dfccc0943b8
Anwesh43 <anweshthecool0@gmail.com>
2019-03-16 15:06:09 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedalldireclineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedalldireclineview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedalldireclineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.alldireclineview.AllDirecLineView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AllDirecLineView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
