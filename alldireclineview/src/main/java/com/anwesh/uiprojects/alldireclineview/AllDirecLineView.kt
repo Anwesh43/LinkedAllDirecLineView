cd9b2584e707de7cd94ffe415d604a3fee950b11
Anwesh43 <anweshthecool0@gmail.com>
2019-03-16 14:11:56 +0530

    adding AllDirecLineView class

--- alldireclineview/src/main/java/com/anwesh/uiprojects/alldireclineview/AllDirecLineView.kt
+++ alldireclineview/src/main/java/com/anwesh/uiprojects/alldireclineview/AllDirecLineView.kt
@@ -53,3 +53,21 @@ fun Canvas.drawADLNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class AllDirecLineView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
