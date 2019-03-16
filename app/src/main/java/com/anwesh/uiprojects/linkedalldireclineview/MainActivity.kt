6aa8f8602d8d5dbc619446afb4d428cbe1b66493
Anwesh43 <anweshthecool0@gmail.com>
2019-03-16 15:03:30 +0530

    adding View in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedalldireclineview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedalldireclineview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedalldireclineview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.alldireclineview.AllDirecLineView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        AllDirecLineView.create(this)
     }
 }
